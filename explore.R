f1f95795c33575c76b1d2d181581995a3ed16926
NoaLRX <noalerouxx@gmail.com>
2024-06-10 13:13:22 +0200

    traduction français-anglaise des commentaires du script exploratoire

--- explore.R
+++ explore.R
@@ -79,27 +79,27 @@ seasonal_combined_test <- c()
 seasonal_seasdum <- c()
 
 for (col_name in names(ts_list)) {
-  ts_name <- paste0("ts_", col_name)  # Nom de la série temporelle ajustée
-  ts_data <- get(ts_name)  # Récupérer les données de la série temporelle ajustée
+  ts_name <- paste0("ts_", col_name)  # Name of adjusted time series
+  ts_data <- get(ts_name)  # Retrieve data from the adjusted time series
   
-  # Appliquer le test combined_test() sur la série temporelle ajustée
+  # Apply the combined_test() test to the adjusted time series
   ct_res <- combined_test(ts_data)
   
-  # Vérifier chaque p-value et afficher les résultats si au moins une valide H1
+  # Check each p-value and display results if at least one valid H1
   ct_results <- ct_res$stat
   ct_pvals <- ct_res$Pval
   if (ct_results == TRUE) {
     cat("\n")
     cat("\n")
-    print(paste0("Résultats du combined_test pour la série ", ts_name))
+    print(paste0("Combined_test results for the series ", ts_name))
     print(ct_res)
     seasonal_combined_test <- c(seasonal_combined_test, ts_name)
   }
   
-  # Appliquer le test seasdum() sur la série temporelle ajustée
+  # Apply the seasdum() test to the adjusted time series
   sd_res <- seasdum(ts_data)
   
-  # Vérifier la p-value et afficher les résultats si elle est < 0.05
+  # Check p-value and display results if < 0.05
   if (sd_res$Pval < 0.05) {
     cat("\n")
     cat("\n")
@@ -109,7 +109,7 @@ for (col_name in names(ts_list)) {
   }
 }
 
-# Afficher les listes des séries présentant de la saisonnalité
+# Display lists of series with seasonality
 print(seasonal_combined_test)
 length(seasonal_combined_test) # 31
 
@@ -117,70 +117,70 @@ print(seasonal_seasdum)
 length(seasonal_seasdum) #38
 
 
-# Calculer les différences entre les deux listes
+# Calculate the differences between the two lists
 only_combined_test <- setdiff(seasonal_combined_test, seasonal_seasdum)
 only_seasdum <- setdiff(seasonal_seasdum, seasonal_combined_test)
 both_tests <- intersect(seasonal_combined_test, seasonal_seasdum)
 
-# Afficher les différences
+# Show differences
 print(only_combined_test)
 print(only_seasdum)
 print(both_tests)
 
 
 
-# Correction de la saisonnalité avec STL pour les séries dans both_tests
+# Seasonality correction with STL for series in both_tests
 for (ts_name in both_tests) {
-  ts_data <- get(ts_name)  # Récupérer les données de la série temporelle
-  decomp <- stl(ts_data[,1], s.window = "periodic")  # Décomposition STL
-  seasonal <- decomp$time.series[, "seasonal"]  # Obtenir la composante saisonnière
-  ts_data_adjusted <- ts_data - seasonal  # Corriger la composante saisonnière
-  assign(ts_name, ts_data_adjusted)  # Mettre à jour la série temporelle ajustée
+  ts_data <- get(ts_name)  # Retrieve time series data
+  decomp <- stl(ts_data[,1], s.window = "periodic")  # STL decomposition
+  seasonal <- decomp$time.series[, "seasonal"]  # Get the seasonal component
+  ts_data_adjusted <- ts_data - seasonal  # Correcting the seasonal component
+  assign(ts_name, ts_data_adjusted)  # Update the adjusted time series
 }
 
 
 
 
-# Vérification de la saisonnalité après correction
+# Checking seasonality after correction
 seasonal_combined_test_after <- c()
 seasonal_seasdum_after <- c()
 
 for (ts_name in both_tests) {
-  ts_data <- get(ts_name)  # Récupérer les données de la série temporelle ajustée
+  ts_data <- get(ts_name)  # Retrieve data from the adjusted time series
   
-  # Appliquer le test combined_test() sur la série temporelle ajustée
+  # Apply the combined_test() test to the adjusted time series
   ct_res <- combined_test(ts_data)
   
-  # Vérifier chaque p-value et afficher les résultats si au moins une est < 0.05
+  # Check each p-value and display results if at least one is < 0.05
   ct_pvals <- ct_res$Pval
   ct_results <- ct_res$stat
   if (ct_results == TRUE) {
-    cat(bold(underline("\033[1m\033[31m", "Résultats du combined_test après correction pour la série ", ts_name, ":\033[0m\n")))
+    cat(bold(underline("\033[1m\033[31m", "Corrected combined_test results for the series ", ts_name, ":\033[0m\n")))
     print(ct_res)
     seasonal_combined_test_after <- c(seasonal_combined_test_after, ts_name)
   }
   
-  # Appliquer le test seasdum() sur la série temporelle ajustée
+  # Apply the seasdum() test to the adjusted time series
   sd_res <- seasdum(ts_data)
   
   # Check and print results if p<0.05
   if (sd_res$Pval < 0.05) {
-    cat(bold(underline("\033[1m\033[31m", "Résultats du seasdum après correction pour la série ", ts_name, ":\033[0m\n")))
+    cat(bold(underline("\033[1m\033[31m", "Results of Seasdum after correction for the TS: ", ts_name, ":\033[0m\n")))
     print(sd_res)
     seasonal_seasdum_after <- c(seasonal_seasdum_after, ts_name)
   }
 }
 
 
-cat(bold("\nSéries présentant encore de la saisonnalité selon combined_test après correction:\n"))
+cat(bold("\nSeries still showing seasonality according to combined_test after correction:\n"))
 print(seasonal_combined_test_after)
 
-cat(bold("\nSéries présentant encore de la saisonnalité selon seasdum après correction:\n"))
+cat(bold("\nSeries still showing seasonality according to seasdum after correction:\n"))
 print(seasonal_seasdum_after)
 
 
 for (x in seasonal_combined_test_after){
-  cat(bold(underline("Résultats des tests pour la série", x,"\n")))
+  cat(bold(underline("Test results for the", x,"\n")))
   print(fried(get(x)))
   cat("\n")
   print(kw(get(x)))
@@ -196,21 +196,21 @@ for (x in seasonal_combined_test_after){
 
 # Stationarity----
 for (i in names(ts_list)) {
-  ts_name <- paste0("ts_", i)  # Nom de la série temporelle
-  adf_result <- adf.test(get(ts_name))  # Appliquer le test ADF
+  ts_name <- paste0("ts_", i)  # Name of the TS
+  adf_result <- adf.test(get(ts_name))  # Apply ADF test for stationnarity
   adf_result
-  if (adf_result$p.value > 0.05) {  # Vérifier la significativité statistique
-    assign(ts_name, diff(get(ts_name)))  # Différencier la série temporelle
-    print(paste("La série", ts_name, "a été différenciée"))
+  if (adf_result$p.value > 0.05) {  # Check if P-Value > 0.05
+    assign(ts_name, diff(get(ts_name)))  # Differentiate between time series
+    print(paste("The TS", ts_name, "has been differentiated"))
   }
 }
 
 for (i in names(ts_list)) {
-  ts_name <- paste0("ts_", i)  # Nom de la série temporelle
-  adf_result <- adf.test(get(ts_name))  # Appliquer le test ADF
+  ts_name <- paste0("ts_", i)  # Name of the TS
+  adf_result <- adf.test(get(ts_name))  # Apply ADF test for stationnarity
   adf_result
-  if (adf_result$p.value > 0.05) {  # Vérifier la significativité statistique
-    print(paste("La série", ts_name, "n'est pas stationnaire"))
+  if (adf_result$p.value > 0.05) {  # Check if P-Value > 0.05
+    print(paste("La série", ts_name, "is not stationary"))
   }
 }
 
