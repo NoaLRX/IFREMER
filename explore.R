e26d2d155ab7f997c6b811296eec18bd8311a440
NoaLRX <noalerouxx@gmail.com>
2024-06-12 12:46:31 +0200

    adding ts prediction models

--- explore.R
+++ explore.R
@@ -6,6 +6,9 @@ library(tseries)
 library(TSA)
 library(RJDemetra)
 library(crayon)
+library(leaps)
+library(gets)
+library(forecast)
 
 # Load data framee
 landingsV2 <- read.csv("Perso/landingsV2.csv")
@@ -276,6 +279,8 @@ for (i in names(ts_list)) {
 
 
 series_list <- ls(pattern = "^ts_.*_adj$")
+
+# Ger rid of ts_name_adj which is not a time serie
 series_list <- series_list[series_list != "ts_name_adj"]
 
 min_length <- min(sapply(mget(series_list), length))
@@ -291,27 +296,292 @@ for (series_name in series_list) {
 
 
 # Atypical points correction AGAIN----
-series_list_adj <- lapply(series_list, function(series_name) {
-  series <- get(series_name)
-  fit <- tso(series)
-  
-  # Vérifier si des outliers ont été détectés
-  if (!is.null(fit$outliers) && nrow(fit$outliers) > 0) {
-    print(paste("Des outliers ont été détectés pour", series_name))
-  }
-  
-  series_adj <- fit$yadj
-  new_name <- paste0(series_name, "_adj")
-  assign(new_name, series_adj, envir = .GlobalEnv)
-  return(series_adj)
-})
+# series_list_adj <- lapply(series_list, function(series_name) {
+#   series <- get(series_name)
+#   fit <- tso(series)
+#   
+#   # Vérifier si des outliers ont été détectés
+#   if (!is.null(fit$outliers) && nrow(fit$outliers) > 0) {
+#     print(paste("Des outliers ont été détectés pour", series_name))
+#   }
+#   
+#   series_adj <- fit$yadj
+#   new_name <- paste0(series_name, "_adj")
+#   assign(new_name, series_adj, envir = .GlobalEnv)
+#   return(series_adj)
+# })
+# 
+# 
+
+
+# Create a data frame with all the time-series----
+# 38 is the lenght of my time series, you have to change it to yours
+# +2 is for the "Year" and éQuarter" columns
+ts_df <- data.frame(matrix(nrow = 38, ncol = length(series_list) + 1))
+names(ts_df) <- c("Time", series_list)
+
+# Get the sequence of quarters
+quarters <- seq(as.Date("2013-07-01"), by = "quarter", length.out = 38)
+
+# Create the "Time" column
+ts_df$Time <- paste(format(quarters, "%Y"), "-", quarters %>% quarter(), sep = "")
+
+# Loop through each time series and add it as a column
+for (i in seq_along(series_list)) {
+  ts_name <- series_list[i]
+  ts_data <- as.numeric(get(ts_name))  # Convert to numeric vector
+  ts_df[, ts_name] <- ts_data
+}
+
+View(ts_df)
+
+
+# Plot
+ggplot(data = ts_df, aes(x = Time, y = ts_JLX_adj, group = 1)) +
+  geom_line()+
+  theme(axis.text.x=element_text(angle=-90, hjust=0.001))
+
+
+# Variables selection with Y=HKE1567----
+## BestSubSet Method----
+
+# Let's get rid of the "Time" column
+ts_df2 <- ts_df[, -1]
+
+# Let's take 80% of the rows of the data frame as training base
+train_size <- floor(0.8 * nrow(ts_df2))
+
+# Let's create the training base
+train <- ts_df2[1:train_size, ]
+
+# Let's apply the variables selection
+leaps <- regsubsets(ts_HKE1567_adj ~ .,data= train, nbest=1, method=c("exhaustive"))
+leaps
+res.sum <- summary(leaps) 
+data.frame(Adj.R2=which.max(res.sum$adjr2),
+           # Cp = RSS/s^2 + N - 2*p, we have more variables than data points
+           # so it's doesn't make sens to use CP
+           CP=which.min(res.sum$cp), 
+           BIC=which.min(res.sum$bic))
+# 2 of the criteria are advising a 8 variables model
+
+#  plot  a  table  of  models  showing  variables  in  each  model 
+plot(leaps,scale="adjr2",main="Adjusted R2")
+# Best Model: ANE, ARA67, JOD, MGR, OCT, POD, SQZ, WEX
+plot(leaps, scale = "bic", main = "BIC")
+# Same
+
+
+
+
+HKE_df <- ts_df %>% 
+  select(ts_HKE1567_adj, ts_ANE_adj, ts_ARA67_adj, ts_JOD_adj, ts_MGR_adj, 
+         ts_OCT_adj, ts_POD_adj, ts_SQZ_adj, ts_WEX_adj)
+
+y_real <- tail(HKE_df$ts_HKE1567_adj, n=8)
+y_real <- as.numeric(y_real)
+
+
+## ARX model with GETS----
+n <- nrow(HKE_df)
+train_size <- round(0.8 * n)
+train_data <- HKE_df[1:train_size, ]
+test_data <- HKE_df[(train_size+1):n, ]
+mX_train <- data.matrix(train_data[,-1])  # every columns except 'HKE'
+mX_test <- data.matrix(test_data[,-1])  # every columns except 'HKE'
+y_train <- train_data$ts_HKE1567_adj
+y_test <- test_data$ts_HKE1567_adj
+model <- arx(y_train, mc = TRUE, ar = 1, mxreg = mX_train, vcov.type = "ordinary")
+n_test <- nrow(test_data)
+p_arxget <- predict(model, n.ahead = n_test, newmxreg = mX_test)
+p_arxget <- as.numeric(p_arxget)
+
+
+# Représentation graphique
+plot(y_real, type = "l", col = "black", lwd = 2, ylim = range(c(p_arxget)))
+# Ajouter les prévisions au graphique
+lines(p_arxget, col = "red", lwd = 2)
+
+rmse <- sqrt(mean((y_real - p_arxget)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 96,84
 
 
 
 
 
 
+## Modele ARX avec auto_arima----
+split <- round(nrow(HKE_df) * 0.8)
+train_df <- HKE_df[1:split, ]
+test_df <- HKE_df[(split+1):nrow(HKE_df), ]
+y_train <- train_df$ts_HKE1567_adj
+xreg_train <- data.matrix(train_df[,-1]) # Exclure la première colonne qui est la variable dépendante
+modelx_train <- auto.arima(y_train, max.q = 0, xreg = xreg_train, seasonal = FALSE, stationary = TRUE)
+xreg_test <- data.matrix(test_df[,-1]) # Exclure la première colonne qui est la variable dépendante
+p_arx <- predict(modelx_train, newxreg = xreg_test, n.ahead = nrow(test_df))$pred
+p_arx <- as.numeric(p_arx)
+
+plot(y_real, type = "l", col = "black", lwd = 2, ylim = range(c(p_arx)))
+lines(p_arxget, col="red")
+lines(p_arx, col = "green",lwd=2)
+
+rmse <- sqrt(mean((y_real - p_arx)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 89.04
 
 
+
+
+## Modele ARMAX avec auto_arima----
+split <- round(nrow(HKE_df) * 0.8)
+train_df <- HKE_df[1:split, ]
+test_df <- HKE_df[(split+1):nrow(HKE_df), ]
+y_train <- train_df$ts_HKE1567_adj
+xreg_train <- data.matrix(train_df[,-1]) # Exclure la première colonne qui est la variable dépendante
+modelx_train <- auto.arima(y_train, xreg = xreg_train, seasonal = FALSE, stationary = TRUE)
+j <- ncol(modelx_train$var.coef)
+tstat <- matrix(nrow=j, ncol=1)
+for(i in 1:j)
+{
+  tstat[i,1] <- modelx_train$coef[i]/sqrt(modelx_train$var.coef[i,i])
+}
+tstat
+xreg_test <- data.matrix(test_df[,-1]) # Exclure la première colonne qui est la variable dépendante
+p_armax <- predict(modelx_train, newxreg = xreg_test, n.ahead = nrow(test_df))$pred
+p_armax <- as.numeric(p_armax)
+p_armax
+
+plot(y_real, type = "l", col = "black",lwd=2, ylim = range(c(p_armax)))
+lines(p_arxget, col="red")
+lines(p_arx, col = "green")
+lines(p_armax, col = "purple",lwd=2)
+
+rmse <- sqrt(mean((y_real - p_armax)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 89.04
+
+
+
+
+
+## Model LM----
+set.seed(123)
+split <- round(nrow(HKE_df) * 0.8)
+train_df <- HKE_df[1:split, ]
+test_df <- HKE_df[(split+1):nrow(HKE_df), ]
+model_lm <- lm(ts_HKE1567_adj ~ ., data = train_df)
+p_lm <- predict(model_lm, newdata = test_df)
+p_lm <- as.numeric(p_lm)
+
+plot(y_real, type = "l", col = "black",lwd=3, ylim = range(c(p_lm)))
+lines(p_arxget, col="red")
+lines(p_arx, col = "green")
+lines(p_armax, col = "purple")
+lines(p_lm, col = "yellow",lwd=3)
+
+rmse <- sqrt(mean((y_real - p_lm)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) #99
+
+
+
+## Model AR1---- 
+y <- ts(HKE_df$ts_HKE1567_adj)
+model_ar1 <- Arima(y, order = c(1, 0, 0), seasonal = list(order = c(0, 0, 0), period=12),lambda=1)
+forecast_ar1 <- forecast(model_ar1, h = 57)
+p_ar1 <- forecast_ar1$mean
+p_ar1 <- as.numeric(p_ar1)
+
+plot(y_test, type = "l", col = "black",lwd=3, ylim = range(c(y_test)))
+lines(p_arxget, col="red")
+lines(p_arx, col = "green")
+lines(p_armax, col = "purple")
+lines(p_lm, col = "yellow")
+lines(p_ar1, col = "pink",lwd=3)
+
+rmse <- sqrt(mean((y_real - p_ar1)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 2.20
+
+
+
+## Model GAM----
+library(mgcv)
+smp_size <- ceiling(0.80 * nrow(HKE_df))
+train_ind <- sample(seq_len(nrow(HKE_df)), size = smp_size)
+train <- HKE_df[train_ind, ]
+test <- HKE_df[-train_ind, ]
+model_gam <- gam(ts_HKE1567_adj ~ s(ts_ANE_adj) + s(ts_ARA67_adj) + s(ts_JOD_adj) + s(ts_MGR_adj) 
+                 + s(ts_OCT_adj) + s(ts_POD_adj) + s(ts_SQZ_adj) + s(ts_WEX_adj), data = train)
+p_gam <- predict(model_gam, newdata = test)
+p_gam <- as.numeric(p_gam)
+
+plot(y_test, type = "l", col = "black",lwd=2, ylim = range(c(y_test)))
+lines(p_arxget, col="red")
+lines(p_arx, col = "green")
+lines(p_armax, col = "purple")
+lines(p_lm, col = "yellow")
+lines(p_ar1, col = "pink")
+lines(p_gam, col="cyan",lwd=2)
+
+rmse <- sqrt(mean((y_real - p_gam)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 101
+
+
+
+
+# Machine-Learning----
+## Modele MLP----
+library(neuralnet)
+set.seed(123) 
+n <- nrow(HKE_df)
+train_size <- round(0.8 * n)
+train <- HKE_df[1:train_size, ]
+test <- HKE_df[(train_size+1):n, ]
+mlp_model <- neuralnet(ts_HKE1567_adj ~ ., data = train, hidden = 1)
+p_mlp <- compute(mlp_model, test[,-1])
+p_mlp <- as.numeric(p_mlp$net.result)
+
+plot(y_real,type = "l", col = "black",lwd=2, ylim = range(c(y_real)))
+lines(p_mlp, col = "brown")
+rmse <- sqrt(mean((y_real - p_mlp)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 57.2
+
+
+
+## Modele MARS----
+library(earth)
+n <- nrow(HKE_df)
+train_size <- round(0.8 * n)
+train <- HKE_df[1:train_size, ]
+test <- HKE_df[(train_size+1):n, ]
+mars_model <- earth(ts_HKE1567_adj ~ ., data = train)
+p_mars <- predict(mars_model, newdata = test)
+p_mars <- as.numeric(p_mars)
+
+plot(y_test, type = "l", col = "black",lwd=2, ylim = range((c(y_real))))
+lines(p_mlp, col = "brown")
+lines(p_mars, col = "cyan",lwd=2)
+
+rmse <- sqrt(mean((y_real - p_mars)^2, na.rm = TRUE))
+print(paste("RMSE :", rmse)) # 36.39
+
+
+
+## Modèle SVM----
+library(e1071)
+library(caret)
+# Trouver les hyper-paramètres optimaux
+svm_grid <- expand.grid(C = c(0.1, 1, 10), sigma = c(0.001, 0.01, 0.1))
+svm_model_opt <- train(ts_HKE1567_adj ~ ., data = train, method = "svmRadial", preProcess = c("center", "scale"), tuneGrid = svm_grid)
+print(svm_model_opt) # On obtient sigma = 0.01 et C = 10.
+
+svm_model_best <- svm(ts_HKE1567_adj ~ ., data = train, kernel = "radial", gamma = 0.1, cost = 0.35, scale = TRUE)
+p_svm <- predict(svm_model_best, newdata = test)
+p_svm <- as.numeric(p_svm)
+
+rmse <- sqrt(mean((y_real - p_svm)^2))
+print(paste("RMSE :", rmse)) # 47
+
+plot(y_test, type = "l", col = "black",lwd=2, ylim = range((c(y_real))))
+lines(p_mlp, col = "brown")
+lines(p_mars, col = "cyan")
+lines(p_svm, col = "darkgreen",lwd=2)
 
 
